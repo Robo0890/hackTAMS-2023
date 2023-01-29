254b11776adbbe7db7ea501eb0ced45c7772ed0c
Ryan <70237244+Robo0890@users.noreply.github.com>
2023-01-28 18:05:38 -0600

    Fix

--- IntelliJ/Orbit/Planet/Planet.gd
+++ IntelliJ/Orbit/Planet/Planet.gd
@@ -18,20 +18,23 @@ export(int, 0, 100) var radius
 export(String) var planet_name
 export(Color) var primary_color
 
+export var test = 0.0
+
 
 func getPositionOnSurface(x : float) -> Vector2:
 	
 	var node = Node2D.new()
-	node.rotation = 180 * x
+	node.rotation_degrees = 360 * x
 	
-	node.translate(node.get_transform().basis_xform(Vector2(0, 50 * radius)))
-	print(node.position)
-	return node.position
+	node.translate(node.get_transform().basis_xform(Vector2(0, -50 * radius)))
+	return node.transform
+	
+
 
 
 func _process(delta):
 	self_modulate = primary_color
-	scale = Vector2(radius, radius)
+	$Planet.scale = Vector2(radius, radius) * 50
 	
-	$Sprite.position = getPositionOnSurface(.5)
+	$Sprite.transform = getPositionOnSurface(test)
 	
